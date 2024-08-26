ab54f15874be917433dcc725f2844ab09f344395
jcsalamanca-uam <jcsalamanca@uamv.edu.ni>
2024-08-26 10:45:27 -0600

    Clase 26/8/24

---
+++ MpeSes3/MpeSes3/Modelos/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpeSes3.Modelos
+{
+    internal class Persona
+    {
+        public Persona() { }
+        public string Nombre { get; set; }
+        public DateTime FechaNac { get; set; }
+        public int CalcularEdad()
+        {
+            int edad = 0;
+            try
+            {
+                edad = DateTime.Now.Year - FechaNac.Year;
+            } catch (Exception ex) 
+            {
+            Console.WriteLine($"OCurrio un error: {ex.Message}");
+            }
+            return edad;
+        }
+        public string EvaluarEdad()
+        {
+            if (CalcularEdad() >= 18) return "Mayor de edad";
+            return "Menor de edad";
+        }
+    }
+}
---
+++ MpeSes3/MpeSes3/Program.cs
@@ -0,0 +1,23 @@
+﻿using MpeSes3.Modelos;
+using System.Runtime.InteropServices;
+
+namespace MpeSes3
+{//Calcular la edad de una persona y determinar si es mayor de edad
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string nombre;
+            DateTime fechaNac;
+            Console.WriteLine("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine ("Fecha de nacimiento: ");
+                fechaNac=DateTime.Parse(Console.ReadLine());
+            persona.Nombre = nombre;
+            persona.FechaNac = fechaNac;
+            Console.WriteLine(persona.EvaluarEdad());
+
+        }
+    }
+}
